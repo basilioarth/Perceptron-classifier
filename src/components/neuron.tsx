2b5dc8b39144311b70de3d469acadf692d31646d
giselesousar <giselesousar@gmail.com>
2021-07-05 19:43:16 -0300

    update changes

--- src/components/neuron.tsx
+++ src/components/neuron.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, Dispatch, SetStateAction } from 'react';
 import { State } from '../models/State';
 import { Neuron } from '../models/Neuron';
+import { Epoch } from '../models/Epoch';
 interface Props {
     states: State[]
     entryBias: number
@@ -16,13 +17,14 @@ export default function NeuronComponent({ states, entryBias, weightBias, thresho
 
     const [neuron] = useState<Neuron>(() => new Neuron(states, threshold, learningRate, entryBias, weightBias, weight_one, weight_two));
 
-    useEffect(() => { 
-        neuron.train(); 
+    const [epochs, setEpochs] = useState<Epoch[]>();
+
+    useEffect(() => {
+        neuron.train();
         const epochs = neuron.getEpochs();
+        setEpochs(epochs);
         const states = epochs[epochs.length - 1].getStates();
         const state = states[states.length - 1];
-
-        console.log(state);
 
         onFinish({
             weight_one: state.weight_one,
@@ -31,11 +33,42 @@ export default function NeuronComponent({ states, entryBias, weightBias, thresho
             isTestReady: true
         })
 
-        console.log(neuron.getEpochs()) 
+        console.log(neuron.getEpochs())
     }, []);
 
     return (
         <div className="Neuron">
+            {
+                epochs?.map((epoch: Epoch, index: number) => (
+                    <div className="card mt-3">
+                        <div className="card-header">
+                            <h4>Época { index + 1 }</h4>
+                        </div>
+                        <div className="card-body">
+                            <div className="row">
+                                {
+                                    epoch.getStates().map((state: State, index: number) => (
+                                        <div className="col-3">
+                                            <ul>
+                                                <h6>Estado {index + 1}</h6>
+                                                <li>x1: {state.entry_one}</li>
+                                                <li>x2: {state.entry_two}</li>
+                                                <li>Bias: {entryBias}</li>
+                                                <li>w1: {state.weight_one}</li>
+                                                <li>w2: {state.weight_two}</li>
+                                                <li>w3: {state.weight_bias}</li>
+                                                <li>Classe esperada: {state.expected_class}</li>
+                                                <li>Classificação: {state.classification}</li>
+                                                <li>Erro: {state.error}</li>
+                                            </ul>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     )
 }
